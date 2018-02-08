2be851a788995f9b5dfe18a13971469ed1a4919a
muted <cle4r@i2pmail.org>
2018-02-08 10:01:55 +0100

    Add basic main install script

---
+++ install.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+set -e
+
+if [ `id -u ` -ne 0 ]; then 
+
+    echo "This script must be run as root" 
+
+    exit 1 
+
+fi
+
+qubes_vm_type="$(qubesdb-read /qubes-vm-type)"
+
+#check in what kind of VM we're runnign
+if [ "$qubes_vm_type" = "TemplateVM" ]; then
+    disclaimer
+    if [ -e /usr/share/anon-gw-base-files/gateway ]; then
+        echo "Running Gateway Template Config script..."
+        ./anon-i2p-gw-template-config
+    elif [ -e /usr/share/anon-ws-base-files/workstation ]; then
+        echo "Running Workstation Template Config script..."
+        ./anon-i2p-ws-template-config
+
+    else 
+        echo "ERROR, couldn't find Whonix files " 
+    fi
+elif [ "$qubes_vm_type" = "ProxyVM"];then
+    if [ -e /usr/share/anon-gw-base-files/gateway ]; then
+        ./anon-i2p-gw-proxyvm-config
+    else    
+        echo "ERROR, couldn't find Whonix files "     
+elif [ "$qubes_vm_type" = "AppVM"; then
+          
+    echo "Running Workstation AppVM Config script..."
+    
+    if [ -e /usr/share/anon-ws-base-files/workstation ]; then
+        ./anon-i2p-ws-appvm-config
+    else    
+        echo "ERROR, couldn't find Whonix files "         
+else
+    echo "Couldn't identify the VM type"    
+fi
